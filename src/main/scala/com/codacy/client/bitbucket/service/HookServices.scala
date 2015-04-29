ac88319464f99cb21c410cec8a12d8be188b43f9
Johann Egger <johann@codacy.com>
2015-04-29 20:23:41 +0300

    delete request implemented

--- src/main/scala/com/codacy/client/bitbucket/service/HookServices.scala
+++ src/main/scala/com/codacy/client/bitbucket/service/HookServices.scala
@@ -8,16 +8,10 @@ class ServiceServices(client: BitbucketClient) {
   def list(author: String, repo: String): RequestResponse[Seq[Service]] =
     AuthorRepoOps(author,repo).list()
 
-  /*def byId(author: String, repo: String, id:Long) = {
-    val resp = AuthorRepoOps(author,repo).list()
-    val value = resp.value.map(_.find(_.id == id))
-    resp.copy(value = value)
-  }*/
-
   def created(author: String, repo: String, payload: Map[String,Seq[String]]) =
     AuthorRepoOps(author,repo).created(payload)
 
-  def removed(author: String, repo: String, id:Long) =
+  def removed(author: String, repo: String, id:Long): RequestResponse[Boolean] =
     AuthorRepoOps(author,repo).removed(id)
 
   private[this] lazy val BASE_URL: String = "https://bitbucket.org/!api/1.0/repositories"
@@ -29,8 +23,9 @@ class ServiceServices(client: BitbucketClient) {
     def created(payload: Map[String,Seq[String]]): RequestResponse[Service] =
       client.post(Request(BASE_URL,classOf[Service]),payload)
 
-    def removed(id:Long): RequestResponse[Boolean] =
-      client.delete(Request(s"%BASE_URL/$id",classOf[Boolean]))
+    def removed(id:Long): RequestResponse[Boolean] = {
+      client.delete(s"$BASE_URL/$id")
+    }
 
     def list(): RequestResponse[Seq[Service]] =
       client.execute(Request(BASE_URL,classOf[Seq[Service]]))
