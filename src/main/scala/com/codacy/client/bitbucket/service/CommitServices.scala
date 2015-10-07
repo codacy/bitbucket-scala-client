b6bc00ef5aa9e9c0385d43549adca90c6bf25e7b
pedrorijo91 <pedrorijo91@gmail.com>
2015-10-07 11:13:31 +0100

    Add comments on changesets
    Added methods for commenting (and deleting comments) on changesets as described in:
    
    * https://confluence.atlassian.com/bitbucket/changesets-resource-296095208.html#changesetsResource-POSTanewcommentonachangeset
    * https://confluence.atlassian.com/bitbucket/changesets-resource-296095208.html#changesetsResource-DELETEacommentonachangeset

--- src/main/scala/com/codacy/client/bitbucket/service/CommitServices.scala
+++ src/main/scala/com/codacy/client/bitbucket/service/CommitServices.scala
@@ -2,16 +2,24 @@ package com.codacy.client.bitbucket.service
 
 import com.codacy.client.bitbucket.CommitComment
 import com.codacy.client.bitbucket.client.{BitbucketClient, Request, RequestResponse}
-import play.api.libs.json.Json
+import play.api.libs.json.{JsNumber, JsObject, JsString}
 
 class CommitServices(client: BitbucketClient) {
 
-  def createComment(author: String, repo: String, commit: String, body: String): RequestResponse[CommitComment] = {
+  def createComment(author: String, repo: String, commit: String, body: String, file: Option[String] = None, line: Option[Int] = None): RequestResponse[CommitComment] = {
     val url = s"https://bitbucket.org/!api/1.0/repositories/$author/$repo/changesets/${commit.take(12)}/comments"
 
-    val values = Json.obj("content" -> body)
+    val params = file.map(filename => "filename" -> JsString(filename)) ++
+      line.map(lineTo => "line_to" -> JsNumber(lineTo))
+
+    val values = JsObject(params.toSeq :+ "content" -> JsString(body))
 
     client.post(Request(url, classOf[CommitComment]), values)
   }
 
+  def deleteComment(author: String, repo: String, commit: String, commentId: Long): Unit = {
+    val url = s"https://bitbucket.org/!api/1.0/repositories/$author/$repo/changesets/${commit.take(12)}/comments/$commentId"
+
+    client.delete(url)
+  }
 }
