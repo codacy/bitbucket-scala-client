9fa2e71c560ab6255c39598e06a9304b6c8fd262
Rafael Cortês <mrfyda@gmail.com>
2016-01-19 22:19:32 +0000

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,6 @@ object Dependencies {
 
   // Generic
   lazy val jodaTime = "joda-time" % "joda-time" % "2.7"
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 
   // Play framework
   lazy val playWS = "com.typesafe.play" %% "play-ws" % "2.4.3"
