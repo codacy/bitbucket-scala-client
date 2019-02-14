24b23e32f1a048f0e72a525749f52ba6cb83e138
João Lopes <jlglopes96@gmail.com>
2019-02-14 15:06:12 +0000

    Update test

--- src/test/scala/com/codacy/client/bitbucket/v2/IssueSpecs.scala
+++ src/test/scala/com/codacy/client/bitbucket/v2/IssueSpecs.scala
@@ -9,88 +9,117 @@ class IssueSpecs extends FlatSpec with Matchers {
     val input =
       """
         |{
-        |	"priority": "major",
-        |	"kind": "bug",
-        |	"repository": {
-        |		"links": {
-        |			"self": {
-        |				"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests"
-        |			},
-        |			"html": {
-        |				"href": "https://bitbucket.org/mrfyda/scalatests"
-        |			},
-        |			"avatar": {
-        |				"href": "https://bytebucket.org/ravatar/%7B75411621-5a51-4bd4-a982-1d86cedd309e%7D?ts=default"
-        |			}
-        |		},
-        |		"type": "repository",
-        |		"name": "ScalaTests",
-        |		"full_name": "mrfyda/scalatests",
-        |		"uuid": "{75411621-5a51-4bd4-a982-1d86cedd309e}"
-        |	},
-        |	"links": {
-        |		"attachments": {
-        |			"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests/issues/1/attachments"
-        |		},
-        |		"self": {
-        |			"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests/issues/1"
-        |		},
-        |		"watch": {
-        |			"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests/issues/1/watch"
-        |		},
-        |		"comments": {
-        |			"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests/issues/1/comments"
-        |		},
-        |		"html": {
-        |			"href": "https://bitbucket.org/mrfyda/scalatests/issues/1/2e95d690c2c58a72e1a0bb4cd76808a4c0c60db4"
-        |		},
-        |		"vote": {
-        |			"href": "https://bitbucket.org/!api/2.0/repositories/mrfyda/scalatests/issues/1/vote"
-        |		}
-        |	},
-        |	"reporter": {
-        |		"username": "mrfyda",
-        |		"display_name": "Rafael Cortês",
-        |		"account_id": "557058:cb3153a1-1a69-4374-a536-fefac8f1cc64",
-        |		"links": {
-        |			"self": {
-        |				"href": "https://bitbucket.org/!api/2.0/users/mrfyda"
-        |			},
-        |			"html": {
-        |				"href": "https://bitbucket.org/mrfyda/"
-        |			},
-        |			"avatar": {
-        |				"href": "https://bitbucket.org/account/mrfyda/avatar/"
-        |			}
-        |		},
-        |		"type": "user",
-        |		"uuid": "{1e960327-38ba-4100-919e-5a677e668cad}"
-        |	},
-        |	"title": "2e95d690c2c58a72e1a0bb4cd76808a4c0c60db4",
-        |	"component": null,
-        |	"votes": 0,
-        |	"watches": 1,
-        |	"content": {
-        |		"raw": "hello world",
-        |		"markup": "markdown",
-        |		"html": "<p>hello world</p>",
-        |		"type": "rendered"
-        |	},
-        |	"assignee": null,
-        |	"state": "new",
-        |	"version": null,
-        |	"edited_on": null,
-        |	"created_on": "2018-09-02T09:08:01.435083+00:00",
-        |	"milestone": null,
-        |	"updated_on": "2018-09-02T09:08:01.435083+00:00",
-        |	"type": "issue",
-        |	"id": 1
+        |    "content": {
+        |        "raw": "",
+        |        "markup": "markdown",
+        |        "html": "",
+        |        "type": "rendered"
+        |    },
+        |    "kind": "task",
+        |    "repository": {
+        |        "name": "test",
+        |        "type": "repository",
+        |        "full_name": "test/test",
+        |        "links": {
+        |            "self": {
+        |                "href": "https://bitbucket.org/!api/2.0/repositories/test/test"
+        |            },
+        |            "html": {
+        |                "href": "https://bitbucket.org/test/test"
+        |            },
+        |            "avatar": {
+        |                "href": "https://bytebucket.org/ravatar/%12345667-d7b9-41c1-9d19-35d012423d0d%7D?ts=default"
+        |            }
+        |        },
+        |        "uuid": "c19f822b-0e29-433a-87a5-ec8ace58aa67"
+        |    },
+        |    "links": {
+        |        "attachments": {
+        |            "href": "https://bitbucket.org/!api/2.0/repositories/test/test/issues/7/attachments"
+        |        },
+        |        "self": {
+        |            "href": "https://bitbucket.org/!api/2.0/repositories/test/test/issues/7"
+        |        },
+        |        "watch": {
+        |            "href": "https://bitbucket.org/!api/2.0/repositories/test/test/issues/7/watch"
+        |        },
+        |        "comments": {
+        |            "href": "https://bitbucket.org/!api/2.0/repositories/test/test/issues/7/comments"
+        |        },
+        |        "html": {
+        |            "href": "https://bitbucket.org/test/test/issues/7/add-code-coverage-to-react"
+        |        },
+        |        "vote": {
+        |            "href": "https://bitbucket.org/!api/2.0/repositories/test/test/issues/7/vote"
+        |        }
+        |    },
+        |    "title": "Add code coverage to React",
+        |    "reporter": {
+        |        "username": "reporter",
+        |        "display_name": "reporter",
+        |        "account_id": "123abc456def789ghi101jkl",
+        |        "links": {
+        |            "self": {
+        |                "href": "https://bitbucket.org/!api/2.0/users/reporter"
+        |            },
+        |            "html": {
+        |                "href": "https://bitbucket.org/reporter/"
+        |            },
+        |            "avatar": {
+        |                "href": "https://bitbucket.org/account/reporter/avatar/"
+        |            }
+        |        },
+        |        "type": "user",
+        |        "nickname": "reporter",
+        |        "uuid": "c19f822b-0e29-433a-87a5-ec8ace58aa67"
+        |    },
+        |    "component": {
+        |        "name": "React",
+        |        "links": {
+        |            "self": {
+        |                "href": "https://bitbucket.org/!api/2.0/repositories/test/test/components/497198"
+        |            }
+        |        }
+        |    },
+        |    "votes": 0,
+        |    "watches": 1,
+        |    "priority": "major",
+        |    "assignee": {
+        |        "username": "jllopes",
+        |        "display_name": "João Lopes",
+        |        "account_id": "123abc456def789ghi101jkl",
+        |        "links": {
+        |            "self": {
+        |                "href": "https://bitbucket.org/!api/2.0/users/jllopes"
+        |            },
+        |            "html": {
+        |                "href": "https://bitbucket.org/jllopes/"
+        |            },
+        |            "avatar": {
+        |                "href": "https://bitbucket.org/account/jllopes/avatar/"
+        |            }
+        |        },
+        |        "type": "user",
+        |        "nickname": "jllopes",
+        |        "uuid": "c19f822b-0e29-433a-87a5-ec8ace58aa67"
+        |    },
+        |    "state": "resolved",
+        |    "version": null,
+        |    "edited_on": null,
+        |    "created_on": "2018-10-01T15:25:53.034773+00:00",
+        |    "milestone": null,
+        |    "updated_on": "2018-10-03T13:19:06.657709+00:00",
+        |    "type": "issue",
+        |    "id": 7
         |}
       """.stripMargin
     val json = Json.parse(input)
     val value = json.validate[Issue]
 
-    value.fold(e => fail(s"$e"), r => r.id shouldBe 1)
+    value.fold(e => fail(s"$e"), r => {
+      r.id shouldBe 7
+      r.reporter.account_id shouldBe "123abc456def789ghi101jkl"
+    })
   }
 
 }
