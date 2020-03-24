01b31941b2204be942d27e1dbded92994754531e
Bruno Ferreira <bruno.ferreira@codacy.com>
2020-03-24 16:34:57 +0000

    feature: Add repository and permissions endpoints CY-1215

--- src/main/scala/com/codacy/client/bitbucket/v2/service/TeamServices.scala
+++ src/main/scala/com/codacy/client/bitbucket/v2/service/TeamServices.scala
@@ -1,12 +1,28 @@
 package com.codacy.client.bitbucket.v2.service
 
-import com.codacy.client.bitbucket.v2.Team
+import com.codacy.client.bitbucket.v2.{Team, UserPermission}
 import com.codacy.client.bitbucket.client.{BitbucketClient, Request, RequestResponse}
 
 class TeamServices(client: BitbucketClient) {
 
+  private val BaseUrl = "https://bitbucket.org/api/2.0/teams"
+
   def list(role: String = "member"): RequestResponse[Seq[Team]] = {
-    client.executePaginated(Request(s"https://bitbucket.org/api/2.0/teams?role=$role", classOf[Seq[Team]]))
+    client.executePaginated(Request(s"$BaseUrl?role=$role", classOf[Seq[Team]]))
+  }
+
+  /**
+    * Retrieve the permissions matching the supplied username and repositorySlug.
+    *
+    * @param username The username or the UUID of the account surrounded by curly-braces.
+    * @param repositorySlug The repository slug or the UUID of the repository surrounded by curly-braces.
+    * @return A [[RequestResponse]] with the user permissions for the repository
+    */
+  def getRepositoryUserPermissions(username: String, repositorySlug: String): RequestResponse[Seq[UserPermission]] = {
+    //TODO: do proper pagination after CY-1210
+    client.executePaginated(
+      Request(s"$BaseUrl/$username/permissions/repositories/$repositorySlug", classOf[Seq[UserPermission]])
+    )
   }
 
 }
