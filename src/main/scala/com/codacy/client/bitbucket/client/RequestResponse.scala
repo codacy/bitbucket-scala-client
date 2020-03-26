55762a425fc48a085617caaf3f87ef1b42bc1ca2
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2020-03-26 18:02:16 +0000

    clean: Fix deprecated version CY-1210

--- src/main/scala/com/codacy/client/bitbucket/client/RequestResponse.scala
+++ src/main/scala/com/codacy/client/bitbucket/client/RequestResponse.scala
@@ -2,12 +2,12 @@ package com.codacy.client.bitbucket.client
 
 sealed trait RequestResponse[+A] {
 
-  @deprecated("This should be removed or re-implemented to support pagination", "11.3.0")
+  @deprecated("This should be removed or re-implemented to support pagination", "12.0.0")
   def map[B](f: A => B): RequestResponse[B] = {
     flatMap(a => RequestResponse.success(f(a)))
   }
 
-  @deprecated("This should be removed or re-implemented to support pagination", "11.3.0")
+  @deprecated("This should be removed or re-implemented to support pagination", "12.0.0")
   def flatMap[B](f: A => RequestResponse[B]): RequestResponse[B] = {
     this match {
       case SuccessfulResponse(a, _, _, _, _, _) => f(a)
@@ -30,13 +30,13 @@ case class FailedResponse(message: String) extends RequestResponse[Nothing]
 
 object RequestResponse {
 
-  @deprecated("Build a SuccessfulResponse object instead of using this", "11.3.0")
+  @deprecated("Build a SuccessfulResponse object instead of using this", "12.0.0")
   def success[A](a: A): RequestResponse[A] = SuccessfulResponse(a)
 
-  @deprecated("Build a FailedResponse object instead of using this", "11.3.0")
+  @deprecated("Build a FailedResponse object instead of using this", "12.0.0")
   def failure[A](message: String): RequestResponse[A] = FailedResponse(message: String)
 
-  @deprecated("This should be either removed or re-implemented to support pagination", "11.3.0")
+  @deprecated("This should be either removed or re-implemented to support pagination", "12.0.0")
   def applyDiscardingPaginationInfo[A](
       r1: RequestResponse[Seq[A]],
       r2: RequestResponse[Seq[A]]
