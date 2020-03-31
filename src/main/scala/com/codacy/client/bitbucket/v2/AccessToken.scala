5caacefb42c154d8896b5613018a7085faafbe27
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2020-03-31 16:14:43 +0100

    feature: Change implementation to match other objects' parsing CY-1241

--- src/main/scala/com/codacy/client/bitbucket/v2/AccessToken.scala
+++ src/main/scala/com/codacy/client/bitbucket/v2/AccessToken.scala
@@ -1,9 +1,19 @@
 package com.codacy.client.bitbucket.v2
 
-import play.api.libs.json.{Format, Json}
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{__, Reads}
 
-case class AccessToken(access_token: String, scopes: String, expires_in: Int, refresh_token: String, token_type: String)
+case class AccessToken(access_token: String, refresh_token: String, expires_in: Int, scopes: String, token_type: String)
 
 object AccessToken {
-  implicit val format: Format[AccessToken] = Json.format[AccessToken]
+
+  // format: off
+  implicit val reader: Reads[AccessToken] = (
+    (__ \ "access_token").read[String] and
+      (__ \ "refresh_token").read[String] and
+      (__ \ "expires_in").read[Int] and
+      (__ \ "scopes").read[String] and
+      (__ \ "token_type").read[String]
+  )(AccessToken.apply _)
+  // format: on
 }
