a2d5303a34b3333d347cb5a3a4e8327c9635101e
João Lopes <lopes@codacy.com>
2021-05-25 16:29:51 +0100

    clean: Take advantage of string interpolation
    Co-authored-by: Lorenzo Gabriele <lorenzolespaul@gmail.com>

--- src/main/scala/com/codacy/client/bitbucket/client/BitbucketClientBase.scala
+++ src/main/scala/com/codacy/client/bitbucket/client/BitbucketClientBase.scala
@@ -256,7 +256,7 @@ abstract class BitbucketClientBase(val client: WSClient, credentials: Credential
         val statusMessage =
           s"""
              |Failed request:
-             |${error.toString}
+             |$error
              |${getFullStackTrace(error)}
           """.stripMargin
         FailedResponse(statusMessage)
