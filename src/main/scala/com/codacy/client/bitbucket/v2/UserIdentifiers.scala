661eabaa0427fee9b7336123cb1add8937a8e632
andrzej.janczak <andrzej.janczak@codacy.com>
2023-10-17 15:40:51 +0200

    fix: filter out users with user.type different from `user`

--- src/main/scala/com/codacy/client/bitbucket/v2/UserIdentifiers.scala
+++ src/main/scala/com/codacy/client/bitbucket/v2/UserIdentifiers.scala
@@ -1,12 +1,3 @@
 package com.codacy.client.bitbucket.v2
 
-import play.api.libs.functional.syntax._
-import play.api.libs.json._
-
-case class UserIdentifiers(account_id: Option[String], uuid: Option[String])
-
-object UserIdentifiers {
-  implicit val reader: Reads[UserIdentifiers] =
-    ((__ \ "user" \ "account_id").readNullable[String] and
-      (__ \ "user" \ "uuid").readNullable[String])(UserIdentifiers.apply _)
-}
+case class UserIdentifiers(account_id: String, uuid: Option[String])
