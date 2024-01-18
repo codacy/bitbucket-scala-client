9c26378d9ecf9d4ebc4b2a1ef2340dcd46372792
manufacturist <15235526+manufacturist@users.noreply.github.com>
2024-01-18 10:46:55 +0200

    feat: expand basecommit fields & add more nullable (ala-797)

--- src/main/scala/com/codacy/client/bitbucket/v2/Account.scala
+++ src/main/scala/com/codacy/client/bitbucket/v2/Account.scala
@@ -6,21 +6,32 @@ import play.api.libs.json._
 import java.time.LocalDateTime
 
 case class Account(
-    uuid: String,
-    username: String,
+    uuid: Option[String],
+    accountId: Option[String],
+    username: Option[String],
     displayName: Option[String],
+    nickname: Option[String],
     avatarLink: Option[String],
-    createdOn: LocalDateTime
-)
+    createdOn: Option[LocalDateTime]
+) {
+
+  def getIdentifier: Option[String] =
+    List(uuid, accountId).find(_.isDefined).flatten
+
+  def getName: Option[String] =
+    List(username, displayName, nickname).find(_.isDefined).flatten
+}
 
 object Account {
 
   implicit val accountReads: Reads[Account] = (
-    (__ \ "uuid").read[String] and
-      (__ \ "username").read[String] and
+    (__ \ "uuid").readNullable[String] and
+      (__ \ "account_id").readNullable[String] and
+      (__ \ "username").readNullable[String] and
       (__ \ "display_name").readNullable[String] and
+      (__ \ "nickname").readNullable[String] and
       (__ \ "links" \ "avatar" \ "href").readNullable[String] and
-      (__ \ "created_on").read[LocalDateTime]
+      (__ \ "created_on").readNullable[LocalDateTime]
   )(Account.apply _)
 
 }
--- src/test/scala/com/codacy/client/bitbucket/v2/CommitSpecs.scala
+++ src/test/scala/com/codacy/client/bitbucket/v2/CommitSpecs.scala
@@ -4,20 +4,32 @@ import com.codacy.client.bitbucket.TestUtils
 import org.scalatest._
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
-class CommitSpecs extends FlatSpec with Matchers {
+class CommitSpecs extends WordSpec with Matchers {
 
-  "Reads[BaseCommit]" should "successfully parse BaseCommit" in {
-    val rawJson = TestUtils.getTestContent("/test-files/get_a_commit.json")
-    val json = Json.parse(rawJson)
-    val value = json.validate[BaseCommit]
+  "Reads[BaseCommit]" should {
 
-    val expectedAvatarLink =
-      "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:3aae1e05-702a-41e5-81c8-f36f29afb6ca/613070db-28b0-421f-8dba-ae8a87e2a5c7/128"
+    "successfully parse default bitbucket response" in {
+      val rawJson = TestUtils.getTestContent("/test-files/get_a_commit_1.json")
+      val json = Json.parse(rawJson)
+      val value = json.validate[BaseCommit]
 
-    value match {
-      case JsSuccess(value, _) => assert(value.author.user.flatMap(_.avatarLink).contains(expectedAvatarLink))
-      case JsError(errors) => fail(s"Failed with$errors")
+      val expectedAvatarLink =
+        "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:3aae1e05-702a-41e5-81c8-f36f29afb6ca/613070db-28b0-421f-8dba-ae8a87e2a5c7/128"
+
+      value match {
+        case JsSuccess(value, _) => assert(value.author.user.flatMap(_.avatarLink).contains(expectedAvatarLink))
+        case JsError(errors) => fail(s"Failed with$errors")
+      }
+    }
+
+    "successfully parse default remote-provider-service integration-test-repo1 repository commit" in {
+      val rawJson = TestUtils.getTestContent("/test-files/get_a_commit_2.json")
+      val value = Json.parse(rawJson).validate[BaseCommit]
+
+      value match {
+        case JsSuccess(value, _) => assert(value.author.user.flatMap(_.nickname).contains("integrationtest"))
+        case JsError(errors) => fail(s"Failed with$errors")
+      }
     }
   }
-
 }
