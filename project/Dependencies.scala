1fbb72e4bca69130089e3f84d65a02afa200f149
Yasmin Zhamborova <yasmin@codacy.com>
2025-06-16 20:39:34 +0200

    [PLUTO-1438] Update versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   def playJson(playJsonVersion: String) = {
     val playwsVersion =
-      if (playJsonVersion.startsWith("2.8.")) "2.1.11"
+      if (playJsonVersion.startsWith("2.8.")) "2.1.1"
       else if (playJsonVersion.startsWith("2.7.")) "2.0.8"
       else sys.error("Missing play-ws version. Check the compatible version based on its pom")
     Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,7 @@ addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.2.0")
 
 addSbtPlugin("com.codacy" % "codacy-sbt-plugin" % "25.2.4")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.3.0")
 
 ThisBuild / libraryDependencySchemes +=
   "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
