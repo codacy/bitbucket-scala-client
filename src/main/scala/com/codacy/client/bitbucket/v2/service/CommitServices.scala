e9d26e49adc9702e8fcbb8dff42eb8d77215998d
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2026-09-16 16:14:05 +0200

    feat: Add getSingleCommitDiff to CommitServices (#156)
    remote-provider-service's getSingleCommitDiff endpoint (codacy/remote-provider-service#460)
    called Bitbucket's diff/spec endpoint directly instead of through this
    client, so it missed the workspace/repository URL encoding that the
    other commit endpoints already apply here.
    
    Also extracts the shared diff URL building (with encoding) into
    generateDiffUrl, used by both getCommitDiff and getSingleCommitDiff.

--- src/main/scala/com/codacy/client/bitbucket/v2/service/CommitServices.scala
+++ src/main/scala/com/codacy/client/bitbucket/v2/service/CommitServices.scala
@@ -47,7 +47,12 @@ class CommitServices(client: BitbucketClient) {
       fromCommitSha: String,
       toCommitSha: String
   ): RequestResponse[String] =
-    client.getRaw(s"${client.repositoriesBaseUrl}/$workspace/$repository/diff/$fromCommitSha..$toCommitSha")
+    client.getRaw(generateDiffUrl(workspace, repository, s"$fromCommitSha..$toCommitSha"))
+
+  // Diffs a single commit spec against its parent, or against an empty tree if it has no parent.
+  // https://developer.atlassian.com/cloud/bitbucket/rest/api-group-commits/#api-repositories-workspace-repo-slug-diff-spec-get
+  def getSingleCommitDiff(workspace: String, repository: String, commitSha: String): RequestResponse[String] =
+    client.getRaw(generateDiffUrl(workspace, repository, commitSha))
 
   def getCommitSha(workspace: String, repository: String, commitSha: String): RequestResponse[SimpleCommitSha] =
     client.execute[SimpleCommitSha](generateCommitUrl(workspace, repository, commitSha))
@@ -63,4 +68,12 @@ class CommitServices(client: BitbucketClient) {
 
   private def generateCommentUrl(author: String, repository: String, commit: String, extraPaths: String*): String =
     generateCommitUrl(author, repository, commit, "comments" +: extraPaths: _*)
+
+  private def generateDiffUrl(workspace: String, repository: String, spec: String): String = {
+    val encodedWorkspace = URLEncoder.encode(workspace, "UTF-8")
+    val encodedRepository = URLEncoder.encode(repository, "UTF-8")
+    val encodedSpec = URLEncoder.encode(spec, "UTF-8")
+
+    s"${client.repositoriesBaseUrl}/$encodedWorkspace/$encodedRepository/diff/$encodedSpec"
+  }
 }
